05a9bff5ed5a2a7359fdaa757676068df462aaf1
Anton <example@gmail.com>
2022-11-18 02:06:54 +0300

    Lesson two

--- js/two_block.js
+++ js/two_block.js
@@ -50,12 +50,99 @@
 
 // console.clear();
 
-function someFunction (a, b) {
-    if (typeof a === 'number' && typeof b === 'number') {
-        return a + b;
-    }
-    return 'One'
-}
-console.log(someFunction(1, 'string'));
+// function someFunction (a, b) {
+//     if (typeof a === 'number' && typeof b === 'number') {
+//         return a + b;
+//     }
+//     return 'One'
+// }
+// console.log(someFunction(1, 'string'));
 
-console.log(!!'sdfgsdg');
+// console.log('sdfgsdg');
+
+// const someString = "Hello";
+// // let someString = "Hello";
+
+// const firstLetter = someString[0];
+// const lastLetter = someString[someString.length -1];
+// console.log(firstLetter, lastLetter);
+
+// const someStringe = function() {
+
+//     const newValu = someString.toUpperCase()
+//     return newValu;
+// }
+
+// console.log(someStringe());
+
+
+
+
+
+// const someStringe = function() {
+
+//     someString = someString.toUpperCase()
+
+//     console.log(someString);
+// }
+
+// someStringe();
+
+// console.log(someString);
+
+
+// const hello = '    helo     oooolo            ';
+
+// const someString = function() {
+//     // console.log(hello);
+//     // return hello.trim();
+//     // return hello.trim();
+
+//     // const newWord = hello.replace('o', 'Y');
+//     const newWord = hello.replaceAll(' ', '').replaceAll('o', 'A');
+//     return newWord;
+
+// }
+
+// console.log(someString())
+
+
+
+// const globalVariable = 'Hello World';
+
+// const someFumctions = function() {
+//     const firstWorld = globalVariable.slice(0, 4);
+//     const firstWorld2 = globalVariable.substring(0, 8);
+//     const firstWorld3 = globalVariable.indexOf('l');
+//     const firstWorld4 = globalVariable.indexOf('l', 4);
+
+//     const firstWorld5 = globalVariable.slice(-globalVariable.length, -2);
+//     const firstWorld6 = globalVariable.slice(-5, 11);
+//     const firstWorld7 = globalVariable.slice(-5, globalVariable.length);
+//     const firstWorld8 = globalVariable.charAt(150);
+
+//     console.log(firstWorld, firstWorld2, firstWorld3, firstWorld4, firstWorld5, firstWorld6, firstWorld7, firstWorld8);
+// }
+
+// someFumctions()
+
+// hello
+// hElLo
+// HELLO
+
+
+const nameWord = 'hEllo';
+
+const someFunction = function() {
+    const firstLetter = nameWord.toLowerCase();
+    // const upperLetter = value.toUpperCase();
+    // const thirdLetter = value.replace();
+
+    return firstLetter;
+}
+
+console.log(someFunction());
+
+
+
+
