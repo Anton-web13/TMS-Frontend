1f4179ce50b1037a8ee8764de08ed9d7f80837fd
Anton-web13 <85175757+Anton-web13@users.noreply.github.com>
2022-12-02 12:57:47 +0300

    Leeson 7 01.12.2022

--- js/two_block.js
+++ js/two_block.js
@@ -1,118 +1,296 @@
-const numbers = [1,2,3,4,5];
-
-// const sum = (numbers) => {
-//     return numbers.reduce((accumulator, number, index, currentArray) => {
-//         console.log('Промежуточное значание', accumulator)
-//         console.log('Тек. значание', number);
-//         console.log('-------------------------------------')
+// const user = {
+//     name: 'Alex',
+//     email: 'alex@dfhtgdfth.com',
+//     address: {
+//         city: 'Minsk',
+//     },
+// }
 //
-//         return accumulator + number;
-//     }, '');
+//
+// const user2 = {
+//     name: 'Oleg',
+//     email: 'dfgsdgfsdf',
+//     address: {
+//         city: 'Minsk',
+//     },
+//     a: 1,
+//     b: 2,
+//     c: 3,
+// }
+
+// const { address: { city: myOwnCity }} = user;
+// const { name, email } = user;
+// const { name: olegNAme, email: olegEmail} = user2;
+
+// const lolKek = {};
+
+// const { a, b, c} = user2;
+// const { email, name, ...lolKek} = user2
+
+// const numbers = [1,2,3,4,5]
+//
+// const [first, second, ...loolKek] = numbers;
+//
+// const [ ...firstBunch ] = numbers
+
+// console.log(firstBunch);
+
+
+// const numberes2 = [1,2,3];
+// const numberes3 = [].concat(numberes2);
+// const user = [{name: 'Alex'}]
+// const numberes3 = [...numberes2];
+//
+// const updateData = user.map(() => {
+//     return {
+//         name: user.name + 1,
+//     }
+// })
+
+// numberes2.push(5);
+
+// console.log(updateData)
+// console.log(numberes3)
+
+// const user = [
+//     {
+//         id: 1,
+//         name: 'Alex'
+//     },
+//     {
+//         id: 2,
+//         name: 'Alex'
+//     },
+//     {
+//         id: 3,
+//         name: 'Alex'
+//     },
+//     {
+//         id: 4,
+//         name: 'Alex'
+//     },
+// ];
+
+
+// const sergsrt = () => {
+//     return user.filter(({id}) => {
+//         return id % 2 === 0
+//     });
+// }
+
+// console.log(sergsrt(user))
+
+
+
+
+
+// const user2 = {
+//     name: 'Alex',
+//     id: 21654651651,
+// }
+//
+// const logName = ({name, id}) => {
+//     return (`Username: ${name}; UserId: ${id}`)
+// }
+//
+// console.log(logName(user2))
+
+
+
+
+// const user95299526 = {
+//     name: 'Alex',
+//     age: 12,
+// }
+
+// const{'age'} = 12,
+
+
+
+
+
+// const letters = ['a','b','c','d','e','f'];
+//
+// const create = (letterList) => {
+//     const result = {};
+//
+//     letterList.forEach((letter) => {
+//         result[letter] = true;
+//     });
+//
+//     return result;
+// }
+//
+//
+// console.log(create(letters))
+
+
+
+
+// const user = {
+//     name: 'Alex',
+//     age: 23,
+//     email: 'sdgfsdgs@srfser.com',
 // };
+
+// const userFields = Object.keys(user);
+// const userValues = Object.values(user);
+// const userEntries = Object.entries(user);
+// const fromEntries = Object.fromEntries(userEntries);
+
+
+
+
+// const replaceKeyValue = (someObject) => {
+//     const result = {};
 //
-// console.log(sum(numbers));
+//     const entries = Object.entries(someObject);
+//
+//     for (const [key, value] of entries) {
+//         console.log(key, value)
+//     }
+//     // console.log(entries)
+//
+//     // const newEntries = entries.map(entry => entry.reverse());
+//     //
+//     // return Object.fromEntries((newEntries));
+//
+//
+//     // for (const [key, value] of entries) {
+//     //     result[value] = key
+//     // }
+//
+//     // for (const entry of entries) {
+//     //     result[entry[1]] = entry[0];
+//     // }
+//
+//     // return result;
+// }
+//
+// console.log(replaceKeyValue(user));
 
 
-// const sum = (numbers) => {
-//     return numbers.reduce((result, number) => {
-//         if (number % 2 === 0) {
-//             result.push(number ** 2)
+// const numc = [1,2,3];
+// numc.reverse();
+// console.log(numc)
+
+
+// console.log(fromEntries);
+
+
+
+
+const users = [
+    {
+        name: "Petr",
+        age: 23,
+        email: "asdasd@mail.com",
+    },
+    {
+        name: "Max",
+        age: 23,
+        email: "asdasd@mail.com",
+    },
+    {
+        name: "Alex",
+        age: 23,
+        email: "asdasd@mail.com",
+    },
+    {
+        name: "Anastasiya",
+        age: 23,
+        email: "asdasd@mail.com",
+    },
+    {
+        name: "Oleg",
+        age: 23,
+        email: "asdasd@mail.com",
+    },
+];
+
+// console.log(users)
+
+
+
+// const someFunction = (users) => {
+//     return users.map(({name, email}) => {
+//         return {
+//             name,
+//             email,
+//         };
+//     });
+// };
+
+// const someFunction = (users) => {
+//     return users.map(({name, email}) => ({name, email}));
+// };
+
+
+
+// const someFunction = (users) => {
+//     return users.map((user, index) => {
+//         if (index % 2 === 0) {
+//             return {
+//                 name: user.name,
+//                 email: user.email,
+//             };
 //         }
 //
-//         // const isNaN = 0;
-//         // if (isNaN === isNaN) {
-//         //     return;
-//         // }
-//
-//         return result;
-//     }, []);
+//         return users;
+//     });
 // };
 //
-// console.log(sum(numbers));
+//
+// console.log(someFunction(users));
 
-const user = {
-    id: 1,
-    name: 'Alex',
-    email: 'lolKek@mail.com',
-    birtYear: 1945,
-    address: {
-        location: {
-            locationLocal: {
-                city: 'Minsk',
-            }
-        }
-        // city: 'Minsk',
-        // country: 'Belarus',
-        // zip: 220130,
-        // street1: 'Pishkina dom Lol',
-        // street2: null,
+
+
+
+const someData = [
+    {
+        id: "asasd123asda",
+        name: "Alex",
     },
-    hobbies: ['Football', 'Codding'],
-    field: {
-        field2: {
-            field3: null,
-        }
+    {
+        id: "dghfghd3134",
+        name: "Maria",
     },
-    location: {},
-    // 1: 25,
-    // 2: 38,
-};
-
-// const city = user.address && user.address.location && user.address.locationLocal && user.address.locationLocal.city;
-// const city2 = user.address?.location?.locationLocal?.city || 'City is not found';
-
-
-const users = {
-    id: 1,
-    name: 'Alex',
-    email: 'lolKek@mail.com',
-    birtYear: 1945,
-    hobbies: ['Football', 'Codding'],
-    address: {
-        city: "Minsk",
-    }
-};
-
-// users.email = "asrfsrgser";
-// users.lasName = "Petrov";
-
-// Object.freeze(users);
-// users.email = "asrfsrgser";
-// users.address.city = "Kiev";
-
-// delete users.id;
-
-
-const user3 = {
-    id: 1,
-    name: 'Alex',
-    email: 'lolKek@mail.com',
-    address: {
-        city: 'Minsk',
+    {
+        id: "ssseeeqwe221",
+        name: "Petr",
     },
+    {
+        id: "ssseeeqwe221333",
+        name: "Anastasiya",
+    },
+];
 
-    sayHello: () => {
-        console.log('Hello');
-    }
-};
 
-// const user2 = user3;
-// user3.id = 2;
-// user2.email = 'anton@mail.com';
+const createIdUserMap = (users) => {
+    // const result = {};
 
-// const user2 = Object.assign({}, user3);
-// const user2 = {...user3}
-const deepClone = JSON.parse(JSON.stringify(user3))
+    // for (const user of users) {
+    //     result[user.id] = user.name;
+    // }
 
-deepClone.address.city = 'Berlin'
-console.log(deepClone);
-console.log(user3);
+    // for (const {id, name} of users) {
+    //     result[id] = name;
+    // }
+    //
+    // return result;
 
-const userNwe = {
-    last: 'NAme'
+    return users.reverse((result, {id, name}) => {
+        result[id] = name;
+
+        return result
+    }, {})
+
 }
 
-const { name, ...restFields } = user3;
-const user4 = { ...userNwe, ...user3 };
+console.log(createIdUserMap(someData))
 
-console.log(user4)
+
+
+
+
+
