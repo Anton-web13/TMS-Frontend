50b8c4f62b0818525ce976320599aa12ca325534
Anton-web13 <85175757+Anton-web13@users.noreply.github.com>
2023-01-10 22:23:28 +0300

    Lesson 14

--- js/two_block.js
+++ js/two_block.js
@@ -1,89 +1,155 @@
-// const createFakeRequest = (response, delay, isFailed = false) => {
-//     return new Promise((resolve, reject) => {
+// const createFakeRequest = (value, delay) => {
+//     return new Promise((resolve) => {
 //         setTimeout(() => {
-//             if (isFailed) {
-//                 reject(new Error("sdfgxdfgdgf"))
-//             }
-//
-//             resolve(response)
-//         }, delay)
-//     })
-// }
-//
-//
-// // const getPokemon = (id) => {
-// //     return fetch(`https://pokeapi.co/api/v2/pokemon/${id}`).then((response) => {
-// //         response.json();
-// //     });
-// // }
-//
-// const loadDate = async () => {
-//     // const pokemons = await createFakeRequest({name: "Pikashu", ability: "Power Shot"}, 5000); // 1m
-//     // const weather = await createFakeRequest({weather: "Sunny", temperatur: "20 deg"}, 5000); // 1m
-//     // const userProfileData = await createFakeRequest({id: "Pikashu", name: "Alex", age: 25}, 5000); // 1m
-//
-//     // console.log({
-//     //     pokemons,
-//     //     weather,
-//     //     userProfileData,
-//     // })
-//
-//     try {
-//         // const [pokemon, weather, userProfileData] = await Promise.allSettled([
-//         //     createFakeRequest({name: "Pikashu", ability: "Power Shot"}, 1000),
-//         //     createFakeRequest({weather: "Sunny", temperatur: "20 deg"}, 3000),
-//         //     createFakeRequest({id: "Pikashu", name: "Alex", age: 25}, 2000)
-//         // ])
-//
-//         // const result = await Promise.race([
-//         //     createFakeRequest({name: "Pikashu", ability: "Power Shot"}, 1000),
-//         //     createFakeRequest({weather: "Sunny", temperatur: "20 deg"}, 3000),
-//         //     createFakeRequest({id: "Pikashu", name: "Alex", age: 25}, 2000)
-//         // ])
-//
-//         const result = await Promise.any([
-//             createFakeRequest({name: "Pikashu", ability: "Power Shot"}, 4000),
-//             createFakeRequest({weather: "Sunny", temperatur: "20 deg"}, 3000),
-//             createFakeRequest({id: "Bulba", name: "Alex", age: 25}, 2000)
-//         ])
-//
-//         // console.log({pokemon, weather, userProfileData});
-//         console.log(result);
-//     } catch(error) {
-//         console.log(error.message)
-//     }
+//             resolve(value);
+//         }, delay);
+//     });
 // };
 //
-// console.log(loadDate());
+// // const loadData = (requestFn, timeout) => {};
+// //
+// // const request = createFakeRequest("Answer from server", 6000);
+// //
+// // loadData(request, 3000);
+//
+// const processRequest = async (requestFn, timeout) => {
+//     const rejection = new Promise((_, reject) => {
+//         setTimeout(() => {
+//             reject(new Error("xdflgkjxhdbfg"));
+//         }, timeout);
+//     });
+//
+//     return Promise.race([requestFn, rejection])
+//         .then((result) => {
+//             console.log(result)
+//         })
+//         .catch((error) => {
+//             console.log(error.message)
+//         })
+// }
+//
+
+
+// const numbersArray = [7, 2, 3, 0, 4, 6, 0, 0, 13, 0, 78, 0, 0, 19, 14];
+//
+// const newArray = (numbers) => {
+//     for (const key in numbers) {
+//         if (key === 0 || key === "0") {
+//             numbers[numbers.length] = key;
+//         }
+//     }
+//
+//     return numbers
+//     // console.log(numbers.length - 1);
+// };
+//
+// console.log(newArray(numbersArray))
+
+
+
+// const dummyData = [7, 2, 3, 0, 4, 6, 0, 0, 13, 0, 78, 0, 0, 19, 14];
+// const solution = (array) => {
+//     const numbers = [];
+//     const zeros = [];
+//
+//     for (const item of array) {
+//         if (item === 0 || item === "0") {
+//             zeros[zeros.length] = item;
+//         } else {
+//             numbers[numbers.length] = item;
+//         }
+//     }
+//
+//     return [...numbers, ...zeros]
+// };
+//
+// console.log(solution(dummyData));
+
+
+
+// const numbers = [5, 8, 6, 3, 4];
+// const oddNumbers = (arr) => {
+//     const odd = arr.filter(i => i%2 !== 0).sort();
+//     let i = 0,
+//         result = [];
+//     arr.forEach(e => {
+//         if (e%2 === 0) {
+//             result.push(e)
+//         } else {
+//             result.push(odd[i]);
+//             i++;
+//         }
+//     });
+//
+//     return result
+// };
+//
+// console.log(oddNumbers(numbers));
+
+
+// const testData = [5, 8, 6, 3, 4];
+//
+// const sortTheOdd = (nums) => {
+//     const evenNums = nums.reduce((result, num, index) => {
+//         if (num % 2 === 0) {
+//             result.push({ value: num, index })
+//         }
+//         return result
+//     }, [])
+//
+//     // return evenNums
+//
+//     const sortedOdds = nums.filter(num => num % 2 !== 0).sort((a, b) => a - b);
+//
+//     evenNums.forEach(({ value, index}) => {
+//         sortedOdds.splice(index, 0, value)
+//     })
+//
+//     return sortedOdds
+// };
+//
+// console.log(sortTheOdd(testData))
 
 
 
 
 
-const solutions = async () => {
+// DOM
 
-    try {
-        const pokemonsShortInfos = await fetch("https://pokeapi.co/api/v2/pokemon").then((response) => {
-            response.json();
-        })
+// const greetinsBlock = document.getElementById("inigueInetifer");
+// // // const greetinsBlock = document.getElementsByClassName()
+// // // const greetinsBlock = document.getElementsByTagName()
+// // // const greetinsBlock = document.querySelector()
+// // // const greetinsBlock = document.querySelectorAll()
+// //
+// console.log(greetinsBlock);
 
-        console.log(pokemonsShortInfos)
+// addEventListener("DOMContentLoaded", (event) => {
+//     const greetinsBlock = document.getElementById("inigueInetifer");
+//     console.log(greetinsBlock);
+// })
 
-    } catch (error) {
-        console.log(error.message)
-    }
-}
-
-console.log(solutions())
-
-
-
-
-
+// for (const item of greetinsBlock) {
+//     console.log(item)
+// }
 
 
+// const greetinsBlock = document.getElementById("greetings");
+// const greetinsBlock = document.getElementsByTagName("div");
 
+// const greetinsBlock = document.querySelector("div")
+// const greetinsBlock = document.querySelectorAll(".greetings")
+// const greetinsBlock = document.querySelectorAll("#inigueInetifer, p")
+const greetinsBlock = document.querySelector(".greetings");
 
+// greetinsBlock.style.background = "black";
+
+// console.log(greetinsBlock.getAttributeNames()[0])
+
+greetinsBlock.classList.remove("anthorBackground");
+// greetinsBlock.classList.add("anthorBackground");
+
+console.log(greetinsBlock);
 
 
 
